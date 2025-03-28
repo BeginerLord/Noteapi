b75857c0afa4230c09618eefb17ff8f8224a6aa2
BeginerLord <133072912+BeginerLord@users.noreply.github.com>
2025-03-28 16:02:20 -0500

    first commit

---
+++ src/main/java/com/appscol/AppsNotasApplication.java
@@ -0,0 +1,13 @@
+package com.appscol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appscol/AppsNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appscol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
